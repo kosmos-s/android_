5e8f3854e285ecbf71cf9000479ce6dfdd3197f3
kosmos-s <kjb50018@gmail.com>
2025-09-16 13:07:26 +0900

    reset to start

---
+++ 3week/app/src/main/java/com/example/my_3week/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.my_3week
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.my_3week.ui.theme.My_3weekTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            My_3weekTheme {
+
+            }
+        }
+    }
+}
